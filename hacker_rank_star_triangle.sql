2e7c1139ce25e2453987efd215d900f4154c5190
sazidshihab <sazidshihab01@gmail.com>
2026-04-08 03:44:14 +0600

    update
    new portion of triangle

--- hacker_rank_star_triangle.sql
+++ hacker_rank_star_triangle.sql
@@ -25,3 +25,31 @@ case when pk between 1 and 19 then '' else pkk end as a_,
 case when pk between 1 and 20 then '' else pkk end as a_
 from cte;
 
+
+
+with recursive cte as(
+    select 1 as pk,"*" as pkk
+    union all
+    select pk+1,"*" as pkk from cte where pk<20
+)select 
+case when pk=1 then pkk else '' end as a_ ,
+case when pk in(1,2) then pkk  else '' end as a_,
+case when pk in(1,2,3) then pkk else '' end as a_ ,
+case when pk between 1 and 4 then pkk else '' end as a_,
+case when pk between 1 and 5 then pkk else '' end as a_,
+case when pk between 1 and 6 then pkk else '' end as a_,
+case when pk between 1 and 7 then pkk else '' end as a_,
+case when pk between 1 and 8 then pkk else '' end as a_,
+case when pk between 1 and 9 then pkk else '' end as a_,
+case when pk between 1 and 10 then pkk else '' end as a_,
+case when pk between 1 and 11 then pkk else '' end as a_,
+case when pk between 1 and 12 then pkk else '' end as a_,
+case when pk between 1 and 13 then pkk else '' end as a_,
+case when pk between 1 and 14 then pkk else '' end as a_,
+case when pk between 1 and 15 then pkk else '' end as a_,
+case when pk between 1 and 16 then pkk else '' end as a_,
+case when pk between 1 and 17 then pkk else '' end as a_,
+case when pk between 1 and 18 then pkk else '' end as a_,
+case when pk between 1 and 19 then pkk else '' end as a_,
+case when pk between 1 and 20 then pkk else '' end as a_
+from cte;
